0be3910d71335cff0e02ce107cae5a59a73d21bd
Alex Benchea <alexbenchea99@yahoo.com>
2019-12-14 16:04:47 +0200

    google maps searchbar + map

--- CleverParking/src/app/_modules/finder/finder.module.ts
+++ CleverParking/src/app/_modules/finder/finder.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { FinderRoutingModule } from './finder-routing'
-
+import {AutocompleteLibModule} from 'angular-ng-autocomplete';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
     CommonModule,
-    FinderRoutingModule
+    FinderRoutingModule,
+    AutocompleteLibModule,
+    HttpClientModule
   ],
   exports: [FinderRoutingModule]
 })
--- CleverParking/src/app/_modules/finder/main/main.component.ts
+++ CleverParking/src/app/_modules/finder/main/main.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements AfterViewInit, OnInit {
+  constructor(private http: HttpClient){
 
-  constructor() { }
+  }
+  title = 'angular-gmap';
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 45.6486;
+  lng = 25.6061;
+  radius = 50000;
 
-  ngOnInit() {
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8
+  };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
+
+  ngAfterViewInit() {
+    this.mapInitializer();
   }
 
+  ngOnInit(){
+    var query = `?location=` + this.lat + "," + this.lng + `
+    &radius=`+ this.radius + `
+    &key=AIzaSyBEgEtjNunnLyyIBVO0ZlCh3gReySJZhkQ
+    &callback=foo`;
+    this.http.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json" + query,
+      ).subscribe(x => console.log(x));
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+      this.mapOptions);
+    this.marker.setMap(this.map);
+  }
+
+  keyword = 'name';
+  data = [
+    {
+      id: 1,
+      name: 'Usa'
+    },
+    {
+      id: 2,
+      name: 'England'
+    }
+  ];
+
+
+  selectEvent(item) {
+    // do something with selected item
+  }
+
+  onChangeSearch(val: string) {
+    // fetch remote data from here
+    // And reassign the 'data' which is binded to 'data' property.
+  }
+
+  onFocused(e) {
+    // do something when input is focused
+  }
 }
