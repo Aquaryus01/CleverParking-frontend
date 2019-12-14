3786251787179e560af6a6b06a8a6f6e0059aa43
condoran <anco990@gmail.com>
2019-12-14 14:33:03 +0200

    findModule

---
+++ CleverParking/src/app/_modules/finder/finder-routing.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, ExtraOptions} from '@angular/router';
+import { MainComponent } from './main/main.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'main', component: MainComponent},
+];
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled'
+};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, routerOptions)],
+  exports: [RouterModule]
+})
+export class FinderRoutingModule { }
--- CleverParking/src/app/app.module.ts
+++ CleverParking/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { FinderModule } from './_modules/finder/finder.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FinderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
